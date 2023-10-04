4f2517402c749e89a9d0efb1b1ffb08c989fecab
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-10-04 11:46:34 -0300

    Criando eventos e funções que determinam qual jogador irá comecar e mostrar o mesmo na tela.

---
+++ script.js
@@ -0,0 +1,58 @@
+//Dados iniciais
+let square = {
+  a1: "",
+  a2: "",
+  a3: "",
+  b1: "",
+  b2: "",
+  b3: "",
+  c1: "",
+  c2: "",
+  c3: "",
+};
+let player = "";
+let warning = "";
+let playing = false;
+
+//Eventos
+document.querySelector(".reset").addEventListener("click", reset);
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("click", itemClick);
+});
+
+//Funções
+function itemClick(event) {
+  let item = event.target.getAttribute("data-item");
+  if (square[item] === "") {
+    square[item] = player;
+    renderSquare();
+  }
+}
+
+function reset() {
+  warning = "";
+
+  let random = Math.floor(Math.random() * 2);
+  player = random === 0 ? "x" : "o";
+
+  for (let i in square) {
+    square[i] = "";
+  }
+
+  palying = true;
+
+  renderSquare();
+  renderInfo();
+}
+
+function renderSquare() {
+  for (let i in square) {
+    let item = document.querySelector(`div[data-item=${i}]`);
+    item.innerHTML = square[i];
+  }
+}
+
+function renderInfo() {
+  document.querySelector(".vez").innerHTML = player;
+  document.querySelector(".resultado").innerHTML = warning;
+}
